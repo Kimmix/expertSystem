6c4e06ded45a9e071a80485b9446d8f68d35b82f
Sarit Soponvattanasevi <kimmix05@gmail.com>
2018-10-23 21:53:44 +0700

    add website

---
+++ README.md
@@ -0,0 +1,18 @@
+#Expert System (pyKnow) and more
+
+Just run
+
+```
+npm install
+```
+
+then
+
+```
+npm start
+```
+
+Expert system project use for 05506210 ARTIFICIAL INTELLIGENCE Course
+
+Bulid with Node.js with Express Server to run PythonShell(main.py)
+include basic html website for query data to run PyKnow
--- node.js
+++ node.js
@@ -1,30 +1,34 @@
-const express = require("express");
-const ps = require("python-shell");
-const morgan = require("morgan");
+const express = require('express');
+const ps = require('python-shell');
+const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/src'));
 
 /* GET home page. */
-app.get("/res/api", (req, res, next) => {
+app.get('/res/api', (req, res, next) => {
   let options = {
-    mode: "text",
-    pythonPath: "./bin/python3",
-    pythonOptions: ["-u"],
-    scriptPath: "./",
+    mode: 'text',
+    pythonPath: './bin/python3',
+    pythonOptions: ['-u'],
+    scriptPath: './',
     args: [req.query.light]
   };
-  ps.PythonShell.run("main.py", options, (err, results) => {
+  ps.PythonShell.run('main.py', options, (err, results) => {
     if (err) throw err;
     // results is an array consisting of messages collected during execution
     console.log(results);
-    res.json({ res: results });
+    res.status(200).json({ res: results[0] });
   });
 });
 
-app.get("/", (req, res) =>
-  res.send("Try: http://localhost:3000/res/api?light=red")
+app.get(
+  '/',
+  (req, res) => res.sendFile(path.join(__dirname + '/src/index.html'))
+  // res.send("Try: http://localhost:3000/res/api?light=red")
 );
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
