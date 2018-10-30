f3c6fd2b14b1a3a090357d3cec3da531b7b9eb3e
Sarit Soponvattanasevi <kimmix05@gmail.com>
2018-10-30 17:59:32 +0700

    base model

--- node.js
+++ node.js
@@ -2,33 +2,38 @@ const express = require('express');
 const ps = require('python-shell');
 const morgan = require('morgan');
 const path = require('path');
-
+const ip = require('ip');
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/src'));
 
-/* GET home page. */
+//GET args then send result
 app.get('/res/api', (req, res, next) => {
   let options = {
-    // mode: 'text',
-    // pythonPath: './bin/python3',
-    // pythonOptions: ['-u'],
-    // scriptPath: './',
     args: [req.query.light]
   };
   ps.PythonShell.run('main.py', options, (err, results) => {
     if (err) throw err;
     // results is an array consisting of messages collected during execution
-    console.log(results);
-    res.status(200).json({ res: results[0] });
+    // console.log(results);
+    res.status(200).json({ res: results });
   });
 });
 
+//Serve webpage
 app.get(
   '/',
   (req, res) => res.sendFile(path.join(__dirname + '/src/index.html'))
   // res.send("Try: http://localhost:3000/res/api?light=red")
 );
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+// app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const port = 3000;
+const localip = ip.address();
+app.listen(port, () =>
+  console.log(
+    '\x1b[35m',
+    `Running on http://localhost:${port} \nor http://${localip}:${port}`
+  )
+);
