5c55a4565495aaae64d30e831500e84f71341835
Sarit Soponvattanasevi <kimmix05@gmail.com>
2018-11-03 17:33:21 +0700

    complete

--- node.js
+++ node.js
@@ -3,8 +3,8 @@ const ps = require('python-shell');
 const morgan = require('morgan');
 const path = require('path');
 const ip = require('ip');
-const app = express();
 
+const app = express();
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/src'));
 
@@ -18,9 +18,10 @@ app.get('/res/api', (req, res, next) => {
       req.query.feature
     ]
   };
+  //Run animal.py
   ps.PythonShell.run('animal.py', options, (err, results) => {
-    if (err) throw err;
     // results is an array consisting of messages collected during execution
+    if (err) throw err;
     console.log(results);
     res.status(200).json({ res: results });
   });
@@ -31,14 +32,7 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname + '/src/index.html'))
 );
 
-//Test if working
-app.get('/test', (req, res) =>
-  res.send(
-    'Try: http://localhost:3000/res/api?mcharaterist=hair&scharaterist=eat_meat&color=white&feature=none'
-  )
-);
-
-// app.listen(3000, () => console.log('Example app listening on port 3000!'));
+//Listen Server
 const port = 3000;
 const localip = ip.address();
 app.listen(port, () =>
